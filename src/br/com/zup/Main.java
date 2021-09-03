f9c11edbc143f856eac07f1e51d6cbeafcfa9ab6
Elaine <elaine.silva@zup.com.br>
2021-09-03 14:54:27 -0300

    "Adicionando a opção de remover cadastro"

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -10,6 +10,12 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Map<String, String> funcionarios = new HashMap<String, String>();
 
+        String nomeFuncionario;
+        String telefoneFuncionario;
+        String emailFuncionario;
+        String cpfFuncionario;
+        String removerFuncionario;
+        boolean funcionarioCadastrado = false;
         boolean finalizar = false;
 
         while (!finalizar) {
@@ -28,44 +34,82 @@ public class Main {
             input.nextLine();
 
             if (opcaoDesejada == 1 ){
-                System.out.println("--------------------------------------------------");
+
                 if (funcionarios.size() == 0){
                     System.out.println("Não foi encontrado nenhum úsuario cadastrado!");
                 }
                 else {
+                    System.out.println("--------------------------------------------------");
                     System.out.println("|             LISTA DE FUNCIONÁRIOS              |");
                     System.out.println("--------------------------------------------------");
                     for (String funcionario : funcionarios.keySet()) {
                         System.out.println("|  " + funcionarios.get(funcionario) + "; CPF: " + funcionario);
                     }
+                    System.out.println("--------------------------------------------------");
+                    System.out.println("Fim de exibição");
                 }
-                System.out.println("--------------------------------------------------");
             }
             else if (opcaoDesejada == 2){
+
                 System.out.println("--------------------------------------------------");
                 System.out.println("|               FICHA DE CADASTRO                |");
                 System.out.println("--------------------------------------------------");
                 System.out.print("|  Digite o nome completo do funcionario: ");
-                String nomeFuncionario = input.nextLine();
+                nomeFuncionario = input.nextLine();
                 System.out.print("|  Digite o telefone do funcionario: ");
-                String telefoneFuncionario = input.nextLine();
+                telefoneFuncionario = input.nextLine();
                 System.out.print("|  Digite o email do funcionario: ");
-                String emailFuncionario = input.nextLine();
+                emailFuncionario = input.nextLine();
                 System.out.print("|  Digite o CPF do funcionario: ");
-                String cpfFuncionario = input.nextLine();
+                cpfFuncionario = input.nextLine();
                 System.out.println("--------------------------------------------------");
 
-                funcionarios.put(cpfFuncionario, "Nome: " + nomeFuncionario + "; Telefone: " + telefoneFuncionario + "; Email: " + emailFuncionario);
+                for (String funcionario : funcionarios.keySet()) {
 
-                System.out.println("Cadastro do funcionario efetuado com sucesso!");
+                    if (cpfFuncionario.equals(funcionario)) {
+                        System.out.println("CPF já cadastrado no sistema!");
+                    }
+                    else {
+                        funcionarioCadastrado = true;
+                    }
+                }
+
+                if (funcionarioCadastrado | funcionarios.size() == 0) {
+
+                    funcionarios.put(cpfFuncionario, "Nome: " + nomeFuncionario + "; Telefone: " + telefoneFuncionario + "; Email: " + emailFuncionario);
+                    System.out.println("Cadastro do funcionario efetuado com sucesso!");
+                }
             }
             else if (opcaoDesejada == 3) {
+
                 System.out.println("--------------------------------------------------");
             }
             else if (opcaoDesejada == 4) {
+
                 System.out.println("--------------------------------------------------");
+                System.out.print("|  Digite o CPF do funcionário que deseja remover: ");
+                removerFuncionario = input.nextLine();
+                System.out.println("--------------------------------------------------");
+
+                if (funcionarios.size() == 0) {
+                    System.out.println("CPF não encontrado!");
+                }
+                else {
+                    for (String funcionario : funcionarios.keySet()) {
+
+                        if (removerFuncionario.equals(funcionario)) {
+                            funcionarios.remove(funcionario);
+                            System.out.println("Funcionario removido com sucesso!");
+                            break;
+                        }
+                        else {
+                            System.out.println("CPF não encontrado!");
+                        }
+                    }
+                }
             }
             else if (opcaoDesejada == 5) {
+
                 System.out.println("PROGRAMA FINALIZADO!");
                 finalizar = true;
             }
